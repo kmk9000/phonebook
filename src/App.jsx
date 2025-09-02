8737579d1a1cb0367edd12354fdad404d7158426
kmk9000 <kallekoivuniemi@gmail.com>
2025-09-02 14:06:03 +0300

    add eventhandlers for name and person object

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,24 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = { name: newName };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
