05945d7b2f13152e0486318435307dd48aa71c8e
kmk9000 <kallekoivuniemi@gmail.com>
2025-09-03 12:34:07 +0300

    add data fetch with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    console.log("Fetching data from server...");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("Data fetched:", response.data);
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
